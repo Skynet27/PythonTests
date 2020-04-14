4a3be673613c4fea5a4380707b4f795a9c3fb3f3
Skynet27 <palcsodavid@gmail.com>
2020-04-14 11:33:15 +0200

    0.0.1.6

--- speech_recognition/alakul.py
+++ speech_recognition/alakul.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 from gtts import gTTS
 import playsound
+import datetime
 
 # va = __import__("my-voice-analysis")
 
@@ -15,11 +16,15 @@ def get_background_noise():
 
 def get_audio():
     with sr.Microphone() as source:
-        audio = r1.listen(source)
-
+        try:
+            audio = r1.listen(source, timeout=3)
+        except:
+            print("(timout)")
+            get_audio()
         try:
             return (r1.recognize_google(audio, language="hu-HU")).lower()
         except:
+            print("(hiba)")
             return "hiba"
 
 
@@ -30,15 +35,26 @@ def speak(text):
 
 
 while True:
-    get_background_noise()
+    #get_background_noise()
     print("Hallgatás...")
     text = get_audio()
     if text.count(name) > 0:
-        speak("Mit szeretnél?")
+        print("Mit szeretnél?")
+        get_background_noise()
         text = get_audio()
         if "semmit" in text:
+            print("Akkor hagylak is!")
             speak("Akkor hagylak is!")
+        elif "mennyi az idő" in text:
+            now = datetime.datetime.now()
+            print(f"Most {now.hour} óra {now.minute} perc van.")
+            speak(f"Most {now.hour} óra {now.minute} perc van.")
+        elif "mondd meg a dátumot" in text:
+            now = datetime.datetime.now()
+            print(f"Ma {now.year} {now.month} hó {now.day} nap van.")
+            speak(f"Ma {now.year} {now.month} hó {now.day} nap van.")
         else:
-            speak("Nem értettem.")
+            print(text)
+            print("Nem értettem.")
     elif text == "hiba":
         print("Hiba lépett fel.")
